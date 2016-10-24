4206fbee7da4f55d8ddc84aa93b98b51d862e339
david.bingjian.yu <david.bingjian.yu@CDC100249>
2016-10-24 18:32:10 +0800

    WindowStyle="None" to set full screen

---
+++ SuperRocket/src/SuperRocket.ModuleTwo/ModuleTwo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.Practices.Unity;
+using Prism.Modularity;
+
+namespace SuperRocket.ModuleTwo
+{
+    public class ModuleTwo : IModule
+    {
+        private readonly IUnityContainer _container;
+
+        public ModuleTwo(IUnityContainer container)
+        {
+            if (container == null)
+            {
+                throw new ArgumentNullException($"{nameof(container)}");
+            }
+
+            _container = container;
+        }
+
+        public void Initialize()
+        {
+            //_container.RegisterType<InterfaceName, ClassName>();
+            System.Windows.MessageBox.Show($"{nameof(ModuleTwo)} has been initialized ;-)");
+        }
+    }
+}

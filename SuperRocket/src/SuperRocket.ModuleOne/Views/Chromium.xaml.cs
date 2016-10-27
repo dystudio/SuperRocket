6d7649b1362c37b60647109e2a54bd7f9f73a06b
david <david.bingjian.yu>
2016-10-27 21:05:39 +0800

    update solution

---
+++ SuperRocket/src/SuperRocket.ModuleOne/Views/Chromium.xaml.cs
@@ -0,0 +1,94 @@
+﻿using CefSharp;
+using CefSharp.Wpf;
+using SuperRocket.ModuleOne.ResourceHandler;
+using System;
+using System.IO;
+using System.Windows.Controls;
+
+namespace SuperRocket.ModuleOne.Views
+{
+    /// <summary>
+    /// Interaction logic for Chromium.xaml
+    /// </summary>
+    public partial class Chromium : UserControl
+    {
+
+        const string defaultUrl = "ids://Modules/{0}/Default.html";
+        private string homePageUrl = string.Empty;
+        public Chromium()
+        {
+            InitializeComponent();
+            InitializeBrowser();
+        }
+
+        private void InitializeBrowser()
+        {
+            var settings = new CefSettings();
+
+
+            settings.RemoteDebuggingPort = 8088;
+
+            settings.RegisterScheme(new CefCustomScheme
+            {
+                SchemeName = "ids",
+                SchemeHandlerFactory = new CefSharpSchemeHandlerFactory()
+                //SchemeHandlerFactory = new InMemorySchemeAndResourceHandlerFactory()
+            });
+
+            Cef.Initialize(settings, performDependencyCheck: true, browserProcessHandler: null);
+
+            var browser = new ChromiumWebBrowser();
+
+            //BrowserSettings browserSettings = new BrowserSettings();
+            ////browserSettings.
+            //browser.BrowserSettings = browserSettings;
+
+
+
+            var handler = browser.ResourceHandlerFactory as DefaultResourceHandlerFactory;
+            if (handler != null)
+            {
+                var moduleName = "Example";//It will be got from the menu click event with module name passed
+                homePageUrl = string.Format(defaultUrl, moduleName);
+                var path = AppDomain.CurrentDomain.BaseDirectory + string.Format(@"Resource\Modules\{0}\Default.html", moduleName);//The path for the home page of the module
+                StreamReader reader = new StreamReader(path, System.Text.Encoding.GetEncoding("utf-8"));
+                var responseBody = reader.ReadToEnd().ToString();
+                reader.Close();
+                var response = CefSharp.ResourceHandler.FromString(responseBody);
+                //response.Headers.Add("HeaderTest1", "HeaderTest1Value");
+                handler.RegisterHandler(homePageUrl, response);
+            }
+
+            browser.LoadError += (sender, args) =>
+            {
+                // Don't display an error for downloaded files.
+                if (args.ErrorCode == CefErrorCode.Aborted)
+                {
+                    return;
+                }
+
+                // Don't display an error for external protocols that we allow the OS to
+                // handle. See OnProtocolExecution().
+                //if (args.ErrorCode == CefErrorCode.UnknownUrlScheme)
+                //{
+                //	var url = args.Frame.Url;
+                //	if (url.StartsWith("spotify:"))
+                //	{
+                //		return;
+                //	}
+                //}
+
+                // Display a load error message.
+                var errorBody = string.Format("<html><body bgcolor=\"white\"><h2>Failed to load URL {0} with error {1} ({2}).</h2></body></html>",
+                                              args.FailedUrl, args.ErrorText, args.ErrorCode);
+
+                args.Frame.LoadStringForUrl(errorBody, args.FailedUrl);
+            };
+            browser.RequestHandler = new RequestHandler();
+            browser.MenuHandler = new MenuHandler();
+            browser.Address = homePageUrl;
+
+            chromiumContainer.Children.Insert(0, browser);
+        }
+    }
+}

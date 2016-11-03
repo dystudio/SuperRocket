85001de4034720d86e15fab4632e8365426aca8f
david <david.bingjian.yu>
2016-11-03 11:23:04 +0800

    test for framework added

--- SuperRocket/src/SuperRocket.SuperChromium/SuperChromiumModule.cs
+++ SuperRocket/src/SuperRocket.SuperChromium/SuperChromiumModule.cs
@@ -3,13 +3,13 @@ using Microsoft.Practices.Unity;
 using Prism.Modularity;
 using SuperRocket.SuperChromium.Services;
 using Autofac;
+using SuperRocket.Framework.Caching;
 
 namespace SuperRocket.SuperChromium
 {
     public class SuperChromiumModule : IModule
     {
         private readonly IContainer _container;
-        
         public SuperChromiumModule(IContainer container)
         {
             if (container == null)
@@ -20,6 +20,7 @@ namespace SuperRocket.SuperChromium
             _container = container;
         }
 
+        public ILifetimeScope LifetimeScope { get; set; }
         public void Initialize()
         {
             var cb = new ContainerBuilder();

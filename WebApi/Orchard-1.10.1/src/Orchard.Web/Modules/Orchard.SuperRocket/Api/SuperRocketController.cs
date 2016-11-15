f2b0b0c79cdd96044c2e4b109054c2e3842dd5e7
david.bingjian.yu <david0718@qq.com>
2016-11-15 18:11:15 +0800

    update

--- WebApi/Orchard-1.10.1/src/Orchard.Web/Modules/Orchard.SuperRocket/Api/SuperRocketController.cs
+++ WebApi/Orchard-1.10.1/src/Orchard.Web/Modules/Orchard.SuperRocket/Api/SuperRocketController.cs
@@ -4,6 +4,7 @@ using Orchard.ContentManagement.MetaData.Models;
 using Orchard.Localization;
 using Orchard.Logging;
 using Orchard.Security;
+using Orchard.SuperRocket.Common;
 using Orchard.SuperRocket.Services;
 using System;
 using System.Collections;
@@ -22,13 +23,17 @@ namespace Orchard.SuperRocket.Api
         private static readonly char[] separator = new[] { '{', '}', ',' };
         private readonly IContentManager _contentManager;
         private readonly IContentTypesService _contentTypesService;
+        private readonly IHtmlModuleService _moduleService;
 
         public SuperRocketController(
             IContentManager contentManager,
-            IContentTypesService contentTypesService)
+            IContentTypesService contentTypesService,
+            IHtmlModuleService moduleService
+            )
         {
             _contentManager = contentManager;
             _contentTypesService = contentTypesService;
+            _moduleService = moduleService;
 
             Logger = NullLogger.Instance;
             T = NullLocalizer.Instance;
@@ -62,6 +67,48 @@ namespace Orchard.SuperRocket.Api
         public IEnumerable<string> GetContentTypes()
         {
             return _contentTypesService.GetContentTypes();
+        }
+
+        /// <summary>
+        /// GET Event/GetContentTypeDefinition
+        /// example: http://localhost/api/SuperRocket/GetAvailableHtmlModules
+        /// </summary>
+        /// <param name="type">Content Type Name</param>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("GetAvailableHtmlModules")]
+        public HttpResponseMessage GetAvailableHtmlModules()
+        {
+            HttpResponseMessage response = new HttpResponseMessage();
+            try
+            {
+                var content = _moduleService.GetAvailableHtmlModules();
+
+                response.Content = Serialize(content, response);
+            }
+            catch (Exception ex)
+            {
+                response.StatusCode = System.Net.HttpStatusCode.BadRequest;
+                Logger.Error("Error occurs when GetAvailableHtmlModules :" + ex.Message);
+            }
+            return response;
+        }
+
+
+        private StringContent Serialize(dynamic source, HttpResponseMessage response)
+        {
+            if (source == null)
+            {
+                response.StatusCode = System.Net.HttpStatusCode.BadRequest;
+            }
+            var settings = new JsonSerializerSettings()
+            {
+                ContractResolver = new NullToEmptyStringResolver(),
+                PreserveReferencesHandling = PreserveReferencesHandling.None
+            };
+
+            var stringcontent = JsonConvert.SerializeObject(source, Newtonsoft.Json.Formatting.Indented, settings);
+            return new StringContent(stringcontent, Encoding.GetEncoding("UTF-8"), "application/json");
         }
     }
 }
---
+++ WebApi/Orchard-1.10.1/src/Orchard.Web/Modules/Orchard.SuperRocket/Services/HtmlModuleService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Orchard.Security;
+using Orchard;
+using Orchard.Logging;
+using Orchard.Localization;
+using Orchard.ContentManagement;
+using Orchard.Core.Title.Models;
+using Orchard.Core.Common.Models;
+using Orchard.Core.Common.Fields;
+
+using Newtonsoft.Json.Linq;
+using System.Text;
+
+namespace Orchard.SuperRocket.Services
+{
+    public class HtmlModuleService : IHtmlModuleService
+    {
+        private readonly IOrchardServices _orchardServices;
+        private readonly IContentManager _contentManager;
+
+        public HtmlModuleService(
+            IOrchardServices orchardServices,
+            IContentManager contentManager) {
+
+            _orchardServices = orchardServices;
+            _contentManager = contentManager;
+
+            Logger = NullLogger.Instance;
+            T = NullLocalizer.Instance;
+        }
+
+        public ILogger Logger { get; set; }
+        public Localizer T { get; set; }
+
+        public dynamic GetAvailableHtmlModules()
+        {
+            var test = _contentManager.Query("HtmlModule").List().Count();
+
+            var modules = _contentManager.Query("HtmlModule").List().Select(contentItem => new
+            {
+                Title = ((dynamic)contentItem).HtmlModulePart.Title.Value
+            });
+            return modules;
+        }
+    }
+}

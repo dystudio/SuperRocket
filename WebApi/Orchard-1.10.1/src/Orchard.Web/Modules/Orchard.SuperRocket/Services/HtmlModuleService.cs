342502aac7ab7f373e260b956c668ead69d22e9a
david.bingjian.yu <david0718@qq.com>
2016-11-16 15:32:55 +0800

    api implement

--- WebApi/Orchard-1.10.1/src/Orchard.Web/Modules/Orchard.SuperRocket/Services/HtmlModuleService.cs
+++ WebApi/Orchard-1.10.1/src/Orchard.Web/Modules/Orchard.SuperRocket/Services/HtmlModuleService.cs
@@ -12,6 +12,10 @@ using Orchard.Core.Common.Fields;
 
 using Newtonsoft.Json.Linq;
 using System.Text;
+using Orchard.Settings;
+using Orchard.Caching;
+using Orchard.Services;
+using Orchard.SuperRocket.Common;
 
 namespace Orchard.SuperRocket.Services
 {
@@ -19,13 +23,25 @@ namespace Orchard.SuperRocket.Services
     {
         private readonly IOrchardServices _orchardServices;
         private readonly IContentManager _contentManager;
+        private readonly ISiteService _siteService;
+        private readonly ICacheManager _cacheManager;
+        private readonly ISignals _signals;
+        private IClock _clock;
 
         public HtmlModuleService(
             IOrchardServices orchardServices,
-            IContentManager contentManager) {
+            IContentManager contentManager,
+            ISiteService siteService,
+            ICacheManager cacheManager,
+            ISignals signals,
+            IClock clock) {
 
             _orchardServices = orchardServices;
             _contentManager = contentManager;
+            _siteService = siteService;
+            _cacheManager = cacheManager;
+            _signals = signals;
+            _clock = clock;
 
             Logger = NullLogger.Instance;
             T = NullLocalizer.Instance;
@@ -36,13 +52,32 @@ namespace Orchard.SuperRocket.Services
 
         public dynamic GetAvailableHtmlModules()
         {
-            var test = _contentManager.Query("HtmlModule").List().Count();
-
-            var modules = _contentManager.Query("HtmlModule").List().Select(contentItem => new
+            var result = _contentManager.Query("HtmlModule").List().Select(contentItem => new
             {
-                Title = ((dynamic)contentItem).HtmlModulePart.Title.Value
+                Title = ((dynamic)contentItem).HtmlModulePart.Title.Value,
+                Author = ((dynamic)contentItem).HtmlModulePart.Author.Value,
+                Url = ((dynamic)contentItem).HtmlModulePart.Url.Value,
+                Description = ((dynamic)contentItem).HtmlModulePart.Description.Value,
+                File = GetBaseUrl() + ((dynamic)contentItem).HtmlModulePart.HtmlModuleFile.FirstMediaUrl ?? string.Empty,
+                Ico = GetBaseUrl() +((dynamic)contentItem).HtmlModulePart.HtmlModuleIco.FirstMediaUrl ?? string.Empty
             });
-            return modules;
+            return result;
+        }
+
+
+        private string GetBaseUrl()
+        {
+            var result = _cacheManager.Get(CacheAndSignals.BaseUrlCache, ctx =>
+            {
+                ctx.Monitor(_clock.When(TimeSpan.FromMinutes(999)));
+                return GetBaseUrlToCache();
+            });
+            return result;
+        }
+
+        private string GetBaseUrlToCache()
+        {
+            return _siteService.GetSiteSettings().BaseUrl;
         }
     }
 }
